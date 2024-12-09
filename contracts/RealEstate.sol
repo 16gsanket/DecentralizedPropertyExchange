a92086f635c818dd95118eda2de373f5ac276132
16gsanket <16gsanket@gmail.com>
2024-12-09 13:21:17 +0530

    added base code

---
+++ contracts/RealEstate.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RealEstate is ERC721URIStorage
+{
+    // used for creating innumerable NFT contracts
+    // Counters.Counter is a struct
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RealEstate","REAL"){}
+    
+    function mint(string memory tokenURI) public returns (uint256){
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+
+        // the order for _mint and _setTokenURI matter as _mint creates a token the _setTokenURI adds metadta to it;
+
+        _mint(msg.sender , newItemId);
+        // _setTokenURI is a prt of ERC721URIStorage
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+
+    function totalSupply() public view returns(uint256){
+        return _tokenIds.current();
+    }
+
+
+}
